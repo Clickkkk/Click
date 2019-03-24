e313d7de12f6e47d1efe79ead2c12e53d8b43bd0
Pasha <Vaspavel6601@gmail.com>
2019-03-24 16:22:25 +0200

    f3

--- app/src/main/java/com/example/pasha/click/MainActivite.java
+++ app/src/main/java/com/example/pasha/click/MainActivite.java
@@ -1,13 +1,24 @@
 package com.example.pasha.click;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivite extends AppCompatActivity {
-
+    private Button play;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activite);
+        play=findViewById(R.id.one);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),PlayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
